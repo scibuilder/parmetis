edfc5ea062b2c458da17bd5095f55aa226929abf
Jack Poulson <poulson@stanford.edu>
2015-05-16 16:25:08 -0700

    Update metisbin.h
    Removing unused ``rint`` definition.

--- metis/programs/metisbin.h
+++ metis/programs/metisbin.h
@@ -44,9 +44,5 @@ extern char* strdup (const char *);
 #endif
 
 #if defined(COMPILER_MSC)
-#if defined(rint)
-  #undef rint
-#endif
-#define rint(x) ((idx_t)((x)+0.5))  /* MSC does not have rint() function */
 #define __func__ "dummy-function"
 #endif

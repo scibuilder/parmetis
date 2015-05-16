8002a84c780d8fb2285ebb448bd180d4849b885e
Jack Poulson <poulson@stanford.edu>
2015-05-16 15:34:08 -0700

    Update gk_arch.h
    Remove the definition of INFINITY since it is not used anywhere in parmetis.

--- metis/GKlib/gk_arch.h
+++ metis/GKlib/gk_arch.h
@@ -61,11 +61,6 @@ typedef ptrdiff_t ssize_t;
 #ifdef __MSC__
 /* MSC does not have rint() function */
 #define rint(x) ((int)((x)+0.5))  
-
-/* MSC does not have INFINITY defined */
-#ifndef INFINITY
-#define INFINITY FLT_MAX
-#endif
 #endif
 
 #endif

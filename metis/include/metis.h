96bafe112d8c9293ebb31a30ca94f7ec070b6ad5
Jack Poulson <poulson@stanford.edu>
2015-05-17 15:59:02 -0700

    Update metis.h
    Avoiding ``METIS_API`` in favor of ``METIS_EXPORT``.

--- metis/include/metis.h
+++ metis/include/metis.h
@@ -168,63 +168,53 @@ typedef __int64 int64_t;
 * Function prototypes 
 *-------------------------------------------------------------------------*/
 
-#ifdef _WINDLL
-#define METIS_API(type) __declspec(dllexport) type __cdecl
-#elif defined(__cdecl)
-#define METIS_API(type) type __cdecl
-#else
-#define METIS_API(type) type
-#endif
-
-
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-METIS_API(int) METIS_PartGraphRecursive(idx_t *nvtxs, idx_t *ncon, idx_t *xadj, 
+METIS_EXPORT int METIS_PartGraphRecursive(idx_t *nvtxs, idx_t *ncon, idx_t *xadj, 
                   idx_t *adjncy, idx_t *vwgt, idx_t *vsize, idx_t *adjwgt, 
                   idx_t *nparts, real_t *tpwgts, real_t *ubvec, idx_t *options, 
                   idx_t *edgecut, idx_t *part);
 
-METIS_API(int) METIS_PartGraphKway(idx_t *nvtxs, idx_t *ncon, idx_t *xadj, 
+METIS_EXPORT int METIS_PartGraphKway(idx_t *nvtxs, idx_t *ncon, idx_t *xadj, 
                   idx_t *adjncy, idx_t *vwgt, idx_t *vsize, idx_t *adjwgt, 
                   idx_t *nparts, real_t *tpwgts, real_t *ubvec, idx_t *options, 
                   idx_t *edgecut, idx_t *part);
 
-METIS_API(int) METIS_MeshToDual(idx_t *ne, idx_t *nn, idx_t *eptr, idx_t *eind, 
+METIS_EXPORT int METIS_MeshToDual(idx_t *ne, idx_t *nn, idx_t *eptr, idx_t *eind, 
                   idx_t *ncommon, idx_t *numflag, idx_t **r_xadj, idx_t **r_adjncy);
 
-METIS_API(int) METIS_MeshToNodal(idx_t *ne, idx_t *nn, idx_t *eptr, idx_t *eind, 
+METIS_EXPORT int METIS_MeshToNodal(idx_t *ne, idx_t *nn, idx_t *eptr, idx_t *eind, 
                   idx_t *numflag, idx_t **r_xadj, idx_t **r_adjncy);
 
-METIS_API(int) METIS_PartMeshNodal(idx_t *ne, idx_t *nn, idx_t *eptr, idx_t *eind,
+METIS_EXPORT int METIS_PartMeshNodal(idx_t *ne, idx_t *nn, idx_t *eptr, idx_t *eind,
                   idx_t *vwgt, idx_t *vsize, idx_t *nparts, real_t *tpwgts, 
                   idx_t *options, idx_t *objval, idx_t *epart, idx_t *npart);
 
-METIS_API(int) METIS_PartMeshDual(idx_t *ne, idx_t *nn, idx_t *eptr, idx_t *eind,
+METIS_EXPORT int METIS_PartMeshDual(idx_t *ne, idx_t *nn, idx_t *eptr, idx_t *eind,
                   idx_t *vwgt, idx_t *vsize, idx_t *ncommon, idx_t *nparts, 
                   real_t *tpwgts, idx_t *options, idx_t *objval, idx_t *epart, 
                   idx_t *npart);
 
-METIS_API(int) METIS_NodeND(idx_t *nvtxs, idx_t *xadj, idx_t *adjncy, idx_t *vwgt,
+METIS_EXPORT int METIS_NodeND(idx_t *nvtxs, idx_t *xadj, idx_t *adjncy, idx_t *vwgt,
                   idx_t *options, idx_t *perm, idx_t *iperm);
 
-METIS_API(int) METIS_Free(void *ptr);
+METIS_EXPORT int METIS_Free(void *ptr);
 
-METIS_API(int) METIS_SetDefaultOptions(idx_t *options);
+METIS_EXPORT int METIS_SetDefaultOptions(idx_t *options);
 
 
 /* These functions are used by ParMETIS */
 
-METIS_API(int) METIS_NodeNDP(idx_t nvtxs, idx_t *xadj, idx_t *adjncy, idx_t *vwgt,
+METIS_EXPORT int METIS_NodeNDP(idx_t nvtxs, idx_t *xadj, idx_t *adjncy, idx_t *vwgt,
                    idx_t npes, idx_t *options, idx_t *perm, idx_t *iperm, 
                    idx_t *sizes);
 
-METIS_API(int) METIS_ComputeVertexSeparator(idx_t *nvtxs, idx_t *xadj, idx_t *adjncy, 
+METIS_EXPORT int METIS_ComputeVertexSeparator(idx_t *nvtxs, idx_t *xadj, idx_t *adjncy, 
                    idx_t *vwgt, idx_t *options, idx_t *sepsize, idx_t *part);
 
-METIS_API(int) METIS_NodeRefine(idx_t nvtxs, idx_t *xadj, idx_t *vwgt, idx_t *adjncy,
+METIS_EXPORT int METIS_NodeRefine(idx_t nvtxs, idx_t *xadj, idx_t *vwgt, idx_t *adjncy,
                    idx_t *where, idx_t *hmarker, real_t ubfactor);
 
 

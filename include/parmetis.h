d2219845f1d763b7d0fa1805b6c3a10626260ece
Jack Poulson <poulson@stanford.edu>
2015-05-17 19:18:48 -0700

    Adding custom vertex separator code from within Elemental

--- include/parmetis.h
+++ include/parmetis.h
@@ -58,56 +58,62 @@ extern "C" {
 /*-------------------------------------------------------------------
 * API Introduced with Release 3.0 (current API) 
 *--------------------------------------------------------------------*/
-METIS_EXPORT int ParMETIS_V3_PartKway(
-             idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt, 
-	     idx_t *adjwgt, idx_t *wgtflag, idx_t *numflag, idx_t *ncon, idx_t *nparts, 
-	     real_t *tpwgts, real_t *ubvec, idx_t *options, idx_t *edgecut, idx_t *part, 
-	     MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V3_PartKway
+(idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt, 
+ idx_t *adjwgt, idx_t *wgtflag, idx_t *numflag, idx_t *ncon, idx_t *nparts, 
+ real_t *tpwgts, real_t *ubvec, idx_t *options, idx_t *edgecut, idx_t *part, 
+ MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_V3_PartGeomKway(
-             idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt, 
-	     idx_t *adjwgt, idx_t *wgtflag, idx_t *numflag, idx_t *ndims, real_t *xyz, 
-	     idx_t *ncon, idx_t *nparts, real_t *tpwgts, real_t *ubvec, idx_t *options, 
-	     idx_t *edgecut, idx_t *part, MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V3_PartGeomKway
+(idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt, 
+ idx_t *adjwgt, idx_t *wgtflag, idx_t *numflag, idx_t *ndims, real_t *xyz, 
+ idx_t *ncon, idx_t *nparts, real_t *tpwgts, real_t *ubvec, idx_t *options, 
+ idx_t *edgecut, idx_t *part, MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_V3_PartGeom(
-             idx_t *vtxdist, idx_t *ndims, real_t *xyz, idx_t *part, MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V3_PartGeom
+(idx_t *vtxdist, idx_t *ndims, real_t *xyz, idx_t *part, MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_V3_RefineKway(
-             idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt, 
-	     idx_t *adjwgt, idx_t *wgtflag, idx_t *numflag, idx_t *ncon, idx_t *nparts, 
-	     real_t *tpwgts, real_t *ubvec, idx_t *options, idx_t *edgecut, 
-	     idx_t *part, MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V3_RefineKway
+(idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt, 
+ idx_t *adjwgt, idx_t *wgtflag, idx_t *numflag, idx_t *ncon, idx_t *nparts, 
+ real_t *tpwgts, real_t *ubvec, idx_t *options, idx_t *edgecut, 
+ idx_t *part, MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_V3_AdaptiveRepart(
-             idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt, 
-	     idx_t *vsize, idx_t *adjwgt, idx_t *wgtflag, idx_t *numflag, idx_t *ncon, 
-	     idx_t *nparts, real_t *tpwgts, real_t *ubvec, real_t *ipc2redist, 
-	     idx_t *options, idx_t *edgecut, idx_t *part, MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V3_AdaptiveRepart
+(idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt, 
+ idx_t *vsize, idx_t *adjwgt, idx_t *wgtflag, idx_t *numflag, idx_t *ncon, 
+ idx_t *nparts, real_t *tpwgts, real_t *ubvec, real_t *ipc2redist, 
+ idx_t *options, idx_t *edgecut, idx_t *part, MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_V3_Mesh2Dual(
-             idx_t *elmdist, idx_t *eptr, idx_t *eind, idx_t *numflag, 
-	     idx_t *ncommonnodes, idx_t **xadj, idx_t **adjncy, MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V3_Mesh2Dual
+(idx_t *elmdist, idx_t *eptr, idx_t *eind, idx_t *numflag, 
+ idx_t *ncommonnodes, idx_t **xadj, idx_t **adjncy, MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_V3_PartMeshKway(
-             idx_t *elmdist, idx_t *eptr, idx_t *eind, idx_t *elmwgt, 
-	     idx_t *wgtflag, idx_t *numflag, idx_t *ncon, idx_t *ncommonnodes, idx_t *nparts, 
-	     real_t *tpwgts, real_t *ubvec, idx_t *options, idx_t *edgecut, idx_t *part, 
-	     MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V3_PartMeshKway
+(idx_t *elmdist, idx_t *eptr, idx_t *eind, idx_t *elmwgt, 
+ idx_t *wgtflag, idx_t *numflag, idx_t *ncon, idx_t *ncommonnodes, idx_t *nparts, 
+ real_t *tpwgts, real_t *ubvec, idx_t *options, idx_t *edgecut, idx_t *part, 
+ MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_V3_NodeND(
-             idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *numflag, 
-	     idx_t *options, idx_t *order, idx_t *sizes, MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V3_NodeND
+(idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *numflag, 
+ idx_t *options, idx_t *order, idx_t *sizes, MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_V32_NodeND(
-             idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt,
-             idx_t *numflag, idx_t *mtype, idx_t *rtype, idx_t *p_nseps, idx_t *s_nseps,
-             real_t *ubfrac, idx_t *seed, idx_t *dbglvl, idx_t *order, 
-             idx_t *sizes, MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_V32_NodeND
+(idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *vwgt,
+ idx_t *numflag, idx_t *mtype, idx_t *rtype, idx_t *p_nseps, idx_t *s_nseps,
+ real_t *ubfrac, idx_t *seed, idx_t *dbglvl, idx_t *order, 
+ idx_t *sizes, MPI_Comm *comm);
 
-METIS_EXPORT int ParMETIS_SerialNodeND(
-             idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *numflag, 
-             idx_t *options, idx_t *order, idx_t *sizes, MPI_Comm *comm);
+METIS_EXPORT int ParMETIS_SerialNodeND
+(idx_t *vtxdist, idx_t *xadj, idx_t *adjncy, idx_t *numflag, 
+ idx_t *options, idx_t *order, idx_t *sizes, MPI_Comm *comm);
+
+METIS_EXPORT void ParMETIS_ComputeVertexSeparator
+(idx_t *vtxdist, idx_t *xadj, idx_t *adjncy,
+ idx_t *p_nseps, idx_t *s_nseps,
+ real_t *ubfrac, idx_t *idbglvl, idx_t *order, idx_t *sizes,
+ MPI_Comm *comm);
 
 #ifdef __cplusplus
 }
